9a60efae9953bf896e596d44f26f30bc6a1fc549
payne-chris-r <06cpayne@gmail.com>
2017-02-23 12:13:34 -0500

    Add route for /ingredients
    Adds route for /ingredients to route to the ingredients controller's
    index action.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: [:index, :show]
+
+  get '/ingredients', to: 'ingredients#index'
 end
